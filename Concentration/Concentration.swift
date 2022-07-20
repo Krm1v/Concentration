27168a2e0519ab474346e8ada6a1de771842c081
Krm1v <vladkarmalov@gmail.com>
2022-07-20 23:40:48 +0200

    - Some model improvements - Added more cards

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -10,9 +10,26 @@ import Foundation
 class Concentration {
     
     var cards = [Card]()
+    var indexOfOneAndOnlyFacedUpCard: Int?
     
     func chooseCard(at index: Int) {
-        cards[index].isFaceUp = cards[index].isFaceUp ? false : true
+        if !cards[index].isMatched {
+            if let matchedIndex = indexOfOneAndOnlyFacedUpCard, matchedIndex != index {
+                if cards[matchedIndex].identifier == cards[index].identifier {
+                    cards[matchedIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFacedUpCard = nil
+            } else {
+                // either no cards or 2 cards are faced up
+                for flipDownIndex in cards.indices {
+                    cards[flipDownIndex].isFaceUp = false
+                }
+                cards[index].isFaceUp = true
+                indexOfOneAndOnlyFacedUpCard = index
+            }
+        }
     }
     
     init(numberOfPairsOfCards: Int) {
